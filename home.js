29afadc03a6e0b59bc6f3fc7f1bcb1073abc217a
tekerene <54807911+tekerene@users.noreply.github.com>
2019-12-23 17:39:10 +0100

    updated file

--- home.js
+++ home.js
@@ -1,27 +1,10 @@
-//console.log("working");
 
-//let name = "rene";
-//let secondName = "technology";
 
-//localStorage.setItem("names",name);
-//localStorage.removeItem('names');
-//localStorage.setItem('second', secondName);
-//localStorage.clear();
-//let localName = localStorage.getItem('names');
-//console.log(localName);
-
-// let names = ['rene', 'teke', 'school', 'johnpaul'];
-// localStorage.setItem('people',JSON.stringify(names));
-// let localName = JSON.parse(localStorage.getItem('people'));
-
-// localName.forEach(element => {
-//     console.log(element);
-// });
 let data = document.querySelector("#data");
 let form = document.querySelector("form");
 let btn = document.querySelector("#btn");
 let ul = document.querySelector("ul");
-//let task = [];
+
 form.addEventListener('submit', (e) => {
 var data = document.querySelector("#data");
   var task;
@@ -34,14 +17,10 @@ var data = document.querySelector("#data");
   task.push(data.value);
    localStorage.setItem('todo list',JSON.stringify(task));
    
- //console.log("task");
-  
     data.value = '';
-    //task.push(data.value);
-    //e.preventDefault();
+    
 });
 let task2 = JSON.parse(localStorage.getItem('todo list'));
-//console.log(task2);
 task2.forEach(ele => {
 let li = document.createElement('li');
 li.innerHTML = `${ele}`;
@@ -51,6 +30,7 @@ console.log(li);
 let button = document.querySelector("#button");
 button.addEventListener("click", (e) => {
 localStorage.clear(button);
+location.reload();
 
 
 });
